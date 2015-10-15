170fefacf9259db4f1396d78b966b0d5957adc0b
unknown <johnbradley2008@gmail.com>
2015-10-14 20:23:04 -0400

    shows list of projects

--- gulpfile.js
+++ gulpfile.js
@@ -229,7 +229,7 @@ gulp.task('clean', function (cb) {
 // Watch files for changes & reload
 gulp.task('serve', ['styles', 'elements', 'images'], function () {
   browserSync({
-    port: 5000,
+    port: 5001,
     notify: false,
     logPrefix: 'PSK',
     snippetOptions: {
